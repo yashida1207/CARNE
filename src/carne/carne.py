40cfbee529594fb6aa521fbf6e491d46d79d715a
AshidaYosuke <assy.8594.1207.physics@gmail.com>
2025-12-23 15:15:39 +0900

    Initial commit

---
+++ src/carne/carne.py
@@ -0,0 +1,50 @@
+"""
+    aaa
+
+"""
+
+import math
+import numpy as np
+import utils
+
+
+##### template from ChatGPT 
+
+class MyClass:
+
+    def __init__(self, value):
+        self.value = value
+
+    def do_something(self):
+        return self.value * 2
+
+#####
+
+
+class wc: 
+    
+    def __init__(self): 
+
+
+    def scale_event(self): 
+
+
+    def set_syst(self): 
+
+
+    def create_toy(self): 
+        
+        return toy_dataset
+
+
+
+
+
+def calc_likelihood(): 
+
+    return loglikeli, teststat
+
+
+
+
+
---
+++ src/carne/utils.py
@@ -0,0 +1,17 @@
+"""
+    aaa
+
+"""
+
+def load_ibd_xsec(): 
+    
+
+
+def interpolate_linear(): 
+
+
+
+def interpolate_log(): 
+
+
+
